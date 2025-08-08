76774b723f7117b8472b919a9758d1e918be5323
zmskv <zmskvxd@gmail.com>
2025-08-08 13:02:34 +0300

    init logger

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,5 +1,11 @@
-package app
+package main
+
+import "github.com/zmskv/order-service/logger"
 
 func main() {
+	log := logger.New()
+	defer log.Sync()
 
+	log.Info("Application started")
+	log.Debug("This is a debug message")
 }
