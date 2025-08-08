54ca52ab4cbff43f28b04b5c74b2920a1a770492
zmskv <zmskvxd@gmail.com>
2025-08-08 13:01:53 +0300

    create order entity

--- backend/internal/domain/order/entity/order.go
+++ backend/internal/domain/order/entity/order.go
@@ -1,1 +1,57 @@
 package entity
+
+import "time"
+
+type Order struct {
+	OrderUID          string
+	TrackNumber       string
+	Entry             string
+	Delivery          Delivery
+	Payment           Payment
+	Items             []Item
+	Locale            string
+	InternalSignature string
+	CustomerID        string
+	DeliveryService   string
+	ShardKey          string
+	SmID              int
+	DateCreated       time.Time
+	OofShard          string
+}
+
+type Delivery struct {
+	Name    string
+	Phone   string
+	Zip     string
+	City    string
+	Address string
+	Region  string
+	Email   string
+}
+
+type Payment struct {
+	Transaction  string
+	RequestID    string
+	Currency     string
+	Provider     string
+	Amount       int
+	PaymentDT    int64
+	Bank         string
+	DeliveryCost int
+	GoodsTotal   int
+	CustomFee    int
+}
+
+type Item struct {
+	ChrtID      int
+	TrackNumber string
+	Price       int
+	RID         string
+	Name        string
+	Sale        int
+	Size        string
+	TotalPrice  int
+	NmID        int
+	Brand       string
+	Status      int
+}
