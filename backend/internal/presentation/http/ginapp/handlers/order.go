6adbbac42865fd1f5524eee2d9c9a60cb73ae09b
zmskv <zmskvxd@gmail.com>
2025-08-08 13:00:46 +0300

    init presentation layer

--- backend/internal/presentation/http/ginapp/handlers/order.go
+++ backend/internal/presentation/http/ginapp/handlers/order.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zmskv/order-service/internal/domain/order/interfaces"
+	"go.uber.org/zap"
+)
+
+type OrderHandler struct {
+	service interfaces.OrderService
+	logger  zap.Logger
+}
+
+func NewOrderHandler(service interfaces.OrderService, logger zap.Logger) *OrderHandler {
+	return &OrderHandler{service: service, logger: logger}
+}
+
+func (h *OrderHandler) GetByID(c *gin.Context) {
+
+}
--- backend/internal/presentation/http/ginapp/routes.go
+++ backend/internal/presentation/http/ginapp/routes.go
@@ -1,1 +1,17 @@
 package ginapp
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zmskv/order-service/internal/domain/order/interfaces"
+	"github.com/zmskv/order-service/internal/presentation/http/ginapp/handlers"
+	"go.uber.org/zap"
+)
+
+func InitRoutes(r *gin.Engine, orderService interfaces.OrderService, logger zap.Logger) {
+	orderHandler := handlers.NewOrderHandler(orderService, logger)
+
+	order := r.Group("/order")
+	{
+		order.GET("/:id", orderHandler.GetByID)
+	}
+}
